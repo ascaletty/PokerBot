17de22714322a79218a8d67c19d9571ed5281c65
ascaletty <144961212+ascaletty@users.noreply.github.com>
2026-03-27 12:52:22 -0500

    Delete target directory

--- target/debug/build/pyo3-macros-backend-cab1eb65b8ba1392/build_script_build-cab1eb65b8ba1392.d
+++
@@ -1,5 +0,0 @@
-/home/ascaletty/projects/poker_server/target/debug/build/pyo3-macros-backend-cab1eb65b8ba1392/build_script_build-cab1eb65b8ba1392.d: /home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-macros-backend-0.27.2/build.rs
-
-/home/ascaletty/projects/poker_server/target/debug/build/pyo3-macros-backend-cab1eb65b8ba1392/build_script_build-cab1eb65b8ba1392: /home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-macros-backend-0.27.2/build.rs
-
-/home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pyo3-macros-backend-0.27.2/build.rs:
--- target/debug/deps/unindent-8ed945ee896cb95c.d
+++
@@ -1,6 +0,0 @@
-/home/ascaletty/projects/poker_server/target/debug/deps/unindent-8ed945ee896cb95c.d: /home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs /home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs
-
-/home/ascaletty/projects/poker_server/target/debug/deps/libunindent-8ed945ee896cb95c.rmeta: /home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs /home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs
-
-/home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs:
-/home/ascaletty/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs:
